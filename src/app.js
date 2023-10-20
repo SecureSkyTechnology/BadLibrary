3b56829a3dd82aeef4f09e9723c8807107d8a601
hasegawa <hasegawa@securesky-tech.com>
2023-10-20 14:15:02 +0900

    問い合わせログのファイル名を修正

--- src/app.js
+++ src/app.js
@@ -313,7 +313,7 @@ const handlers = [
             d.getFullYear().toString().padStart(4, '0') +
             (d.getMonth() + 1).toString().padStart(2, '0') +
             d.getDate().toString().padStart(2, '0')
-          const filename = path.resolve(__dirname, 'log', `/${date}.txt`)
+          const filename = path.resolve(__dirname, 'log/', `${date}.txt`)
           if (config.vulnerabilities.expose.indexOf('contact') >= 0) {
             fs.open(filename, 'a', (err, fd) => {
               if (err) {
